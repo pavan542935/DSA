8c7301a02d8016fd9166b036f8ef395ae25ccd2a
pavan542935 <139714985+pavan542935@users.noreply.github.com>
2026-08-26 12:27:38 +0530

    Time: 0 ms (100%), Space: 19.2 MB (90.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,9 +3,6 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
 
-        if len(nums) == 2:
-            return max(nums[0],nums[1])
-
         n = len(nums)
         dp = [0]*(n+1)
 
